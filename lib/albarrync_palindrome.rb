6424252483c10229324401399f487ac0a09d8b72
Alex Barry <albarrync@gmail.com>
2020-03-29 23:29:38 -0400

    Add punctuation support for palindrome detector

--- lib/albarrync_palindrome.rb
+++ lib/albarrync_palindrome.rb
@@ -1,6 +1,27 @@
-require "albarrync_palindrome/version"
+# frozen_string_literal: true
 
-module AlbarryncPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+require 'albarrync_palindrome/version'
+
+# Modifiy native String to include a method for detecting palindromes.
+class String
+  # Method determins if reverse is equal to input.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns the letters of a string.
+  def letters
+    the_letters = []
+    (0..length - 1).each do |i|
+      the_letters << self[i] if self[i].match(/[a-zA-Z]/)
+    end
+    the_letters.join
+  end
+
+  private
+
+  # Returns processed palindrome for detecting method.
+  def processed_content
+    letters.downcase
+  end
 end
--- test/albarrync_palindrome_test.rb
+++ test/albarrync_palindrome_test.rb
@@ -1,11 +1,29 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class AlbarryncPalindromeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::AlbarryncPalindrome::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_non_palindrome
+    refute 'apple'.palindrome?
+  end
+
+  def test_simple_palindrome
+    assert 'racecar'.palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert 'RaceCar'.palindrome?
+  end
+
+  def test_punctuated_palindrome
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  def test_letters
+    assert_equal 'MadamImAdam', "Madam, I'm Adam.".letters
   end
 end
